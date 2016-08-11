2270654107cc98d5508e7b018e472c5083e2839c
jurate162001 <jurate162001@ufl.edu>
2016-08-11 11:58:09 -0400

    update anthro script for KAs to use

--- ANTHRO_SCRIPT_08092016.R
+++ ANTHRO_SCRIPT_08092016.R
@@ -30,6 +30,11 @@ anthro <- function() {
     install.packages("ggplot2") 
   } 
   library(ggplot2)
+ 
+  if (!require("RMySQL")) {
+    install.packages("RMySQL")
+  }
+  library(RMySQL)
   
   
   print("Input the four letters that signify the patient we are doing calculations for") 
@@ -2096,6 +2101,53 @@ png <- "ANTHROPOMETRICS_GRAPH.png"
 png <- gsub(" ","", paste(patient,"_", png))
 ggsave(anthrograph, file=png, height=4.5, width=6.61, units='in', dpi=600)
 
+#function to upload anthros into MySQL
+uploadanthros <- function(finaltable){
+  
+all_cons <- dbListConnections(MySQL())
+for (con in all_cons) {
+  dbDisconnect(con)
+}
+
+# Input MySQL user name, password, database name, and host
+# Then establish a connection to the database
+print("When prompted, please input the MySQL username, password, database name, and host that you wish to use")
+print("To leave any of these fields blank, press the enter key without typing anything else")
+u <- readline(prompt="User: ")
+p <- readline(prompt="Password: ")
+d <- readline(prompt="Database Name: ")
+h <- readline(prompt="Host: ")
+connect <- dbConnect(MySQL(),user=u,password=p,dbname=d,host=h)
+
+dbSendQuery(connect,paste("USE",d))
+
+mrnumber <- unique(finaltable$MRNUMBER)
+table <- "anthropometrics_id_research"
+
+exists <- dbGetQuery(connect,paste("SHOW TABLES FROM",d,"LIKE",gsub(" ","",paste("'",table,"'")),";"))
+if (length(exists[,1])==1) {
+  exists.this <- dbGetQuery(connect,paste("SELECT * FROM",table,"WHERE MRNUMBER=",mrnumber,";"))
+  if (dim(exists.this)[1]>0) {
+    print(paste("Data for patient with mrnumber",mrnumber,"already exists in table",
+                gsub(" ","",paste(table,".")),"Would you like to update it?"))
+    r <- " "
+    while (tolower(r)!="yes" & tolower(r)!="no") {
+      r <- readline(prompt="Type 'yes' or 'no': ")
+    }
+    if (tolower(r)=="yes") {
+      dbSendQuery(connect,paste("DELETE FROM",table,"WHERE MRNUMBER=",mrnumber,";"))
+      dbWriteTable(connect,name=table,value=finaltable,append=TRUE)
+    }
+  } else {
+    dbWriteTable(connect,name=table,value=finaltable,append=TRUE)
+  }
+  print(paste("Table",table,"has been updated with data for this patient"))
+} else if (length(exists[,1])==0) {
+  dbWriteTable(connect,name=table,value=finaltable,append=TRUE)
+  print(paste("Table",table,"has been created and can be found in the database"))
+}
+}
+
 #upload to MySQL function, need to run upload_anthros_database first
 database <- function() {
   print("Would you like to upload anthropometrics data into a MySQL database?")
@@ -2110,7 +2162,6 @@ database <- function() {
   }
 }
 database()
-
 
 #use rm below to clean working directory so that you can automatically go to next patient?
 rm(list=ls())
